0591edcc934fcf86e502dc8ddd1b1fb37e0e5ef5
Mathias2860DK <mathias.enemark.poulsen@gmail.com>
2022-04-18 21:16:15 +0200

    cinemachine + sensitivity + start crosshair

---
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using StarterAssets;
+
+public class ThirdPersonShooterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] private float normalSensitivity;
+    [SerializeField] private float aimSensitivity;
+
+    private StarterAssetsInputs starterAssetsInputs;
+    private ThirdPersonController thirdPersonController;
+
+    private void Awake()
+    {
+        starterAssetsInputs = GetComponent<StarterAssetsInputs>();
+        thirdPersonController = GetComponent<ThirdPersonController>();
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (starterAssetsInputs.aim)
+        {
+            thirdPersonController.SetSensitivity(aimSensitivity);
+            aimVirtualCamera.gameObject.SetActive(true);
+        }
+        else
+        {
+            aimVirtualCamera.gameObject.SetActive(false);
+            thirdPersonController.SetSensitivity(normalSensitivity);
+        }
+    }
+}
